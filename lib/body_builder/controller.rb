2b92360c650700e77d7a26e840cc0417d934b396
Evgeny Kungurov <evgenii.kungurov@gmail.com>
2016-01-11 23:18:05 +0800

    add new

--- lib/body_builder/controller.rb
+++ lib/body_builder/controller.rb
@@ -1,16 +1,10 @@
 require 'erubis'
-<<<<<<< HEAD
 require 'body_builder/action_notice'
 
 module BodyBuilder
   class Controller
     include BodyBuilder::ActionNotice
 
-=======
-
-module BodyBuilder
-  class Controller
->>>>>>> 384f7320b0d5e76cb77f892ee5632e21e00535e0
     attr_reader :request
 
     def initialize(env)
@@ -22,15 +16,10 @@ module BodyBuilder
     end
 
     def response
-<<<<<<< HEAD
-
     end
 
     def redirect_to(action, args = nil)
       self.send(action)
-=======
-      
->>>>>>> 384f7320b0d5e76cb77f892ee5632e21e00535e0
     end
 
     def render(views_action, options = {})
